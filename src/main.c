66747cf06615dc7073ba061332eecf79a94a5a5f
Tyarel8 <98483313+Tyarel8@users.noreply.github.com>
2022-05-09 19:55:49 +0200

    añadidos comentarios

--- src/main.c
+++ src/main.c
@@ -1,36 +1,10 @@
 /*
-          _____                    _____                    _____                    _____
-         /\    \                  /\    \                  /\    \                  /\    \
-        /::\    \                /::\____\                /::\    \                /::\    \
-       /::::\    \              /:::/    /               /::::\    \              /::::\    \
-      /::::::\    \            /:::/    /               /::::::\    \            /::::::\    \
-     /:::/\:::\    \          /:::/    /               /:::/\:::\    \          /:::/\:::\    \
-    /:::/__\:::\    \        /:::/    /               /:::/__\:::\    \        /:::/__\:::\    \
-   /::::\   \:::\    \      /:::/    /               /::::\   \:::\    \      /::::\   \:::\    \
-  /::::::\   \:::\    \    /:::/    /      _____    /::::::\   \:::\    \    /::::::\   \:::\    \
- /:::/\:::\   \:::\    \  /:::/____/      /\    \  /:::/\:::\   \:::\____\  /:::/\:::\   \:::\    \
-/:::/  \:::\   \:::\____\|:::|    /      /::\____\/:::/  \:::\   \:::|    |/:::/__\:::\   \:::\____\
-\::/    \:::\  /:::/    /|:::|____\     /:::/    /\::/   |::::\  /:::|____|\:::\   \:::\   \::/    /
- \/____/ \:::\/:::/    /  \:::\    \   /:::/    /  \/____|:::::\/:::/    /  \:::\   \:::\   \/____/
-          \::::::/    /    \:::\    \ /:::/    /         |:::::::::/    /    \:::\   \:::\    \
-           \::::/    /      \:::\    /:::/    /          |::|\::::/    /      \:::\   \:::\____\
-           /:::/    /        \:::\__/:::/    /           |::| \::/____/        \:::\   \::/    /
-          /:::/    /          \::::::::/    /            |::|  ~|               \:::\   \/____/
-         /:::/    /            \::::::/    /             |::|   |                \:::\    \
-        /:::/    /              \::::/    /              \::|   |                 \:::\____\
-        \::/    /                \::/____/                \:|   |                  \::/    /
-         \/____/                  ~~                       \|___|                   \/____/
-
-   Programa de ejemplo para la AureBoard
-   Requerido: Aureboard Versi�n>2.0
-
-   - El programa configura el puerto Serie por USB.
-   - Cada programa deber� ir en su carpeta al mismo nivel de la carpeta lib
-
-
-
-
-   A.U.R.E. 2010
+    ___                   __          __     ______                          ___
+   /   | __  __________  / /_  ____  / /_   / ____/______  ______  ____     <  /
+  / /| |/ / / / ___/ _ \/ __ \/ __ \/ __/  / / __/ ___/ / / / __ \/ __ \    / / 
+ / ___ / /_/ / /  /  __/ /_/ / /_/ / /_   / /_/ / /  / /_/ / /_/ / /_/ /   / /  
+/_/  |_\__,_/_/   \___/_.___/\____/\__/   \____/_/   \__,_/ .___/\____/   /_/   
+                                                         /_/                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
 */
 
 #include <../lib/aurebot.h>
@@ -43,69 +17,24 @@
 #include <../lib/sonido.h>
 #include <../lib/2leds.h>*/
 
+// Inicio del display, usando la funcion lcd_gotoxy(iniciox,inicioy) se mueve el cursor al inicio
 #define iniciox 0b1
 #define inicioy 0b1
 
+// Pines para la lectura de los diferentes dispositivos
 #define PIN_BUMPER PIN_A0
 #define PIN_INF PIN_A1
-
-// Rutina de gesti�n de pulsaciones serie
-void aure_serie()
-{
-   output_toggle(LED);
-   switch(keypress)
-   {
-//Control de movimiento
-            case 'w':
-               motores_palante();
-               break;
-            case 'W':
-               motores_palante();
-               break;
-            case 's':
-               motores_patras();
-               break;
-            case 'S':
-               motores_patras();
-               break;
-            case 'd':
-               motores_paderecha();
-               break;
-            case 'D':
-               motores_paderecha();
-               break;
-            case 'a':
-               motores_paizda();
-               break;
-            case 'A':
-               motores_paizda();
-               break;
-            case 'q':
-               motores_parar();
-               break;
-            case 'Q':
-               motores_parar();
-               break;               
-            default:
-               printf (usb_cdc_putc, "NR:%c\r\n", keypress);
-               break;
-   }
-   //Vaciamos el buffer
-   keypress=0;
-}
-
-
 
 //Rutina principal
 void main()
 {
-   int i, d, j, k;
+   // Inicializacion de los dispositivos
    aure_configurar();
    lcd_init();
    lcd_configurar();
    aure_configurar_usb_sinespera();
    
-   for(i=0; i<50; i++)
+   for(int i=0; i<50; i++)
    {
      delay_ms(100);
      if(input(PULSADOR))i=100;
@@ -114,18 +43,18 @@ void main()
    if (aure_configurar_usb_comprobar()==1) 
    {
    
-      // Si est� conectado
+      // Si esta conectado
          while (!input(PULSADOR)) {aure_usb();} //Esperamos hasta que se pulse el pulsador 
          aure_usb();
          while (1) {aure_usb();}
    } else {
-      // Si no est� conectado   
+      // Si no esta conectado   
          while (!input(PULSADOR)) {} //Esperamos hasta que se pulse el pulsador 
-         for (i=0; i<2; i++) {
+         for (i=0; i<2; i++) { // Bucle para detectar obstaculo 2 veces
          printf(lcd_putc,"Palante            ");
          lcd_gotoxy(iniciox,inicioy);
          motores_palante();
-         while (input(PIN_BUMPER)) {
+         while (input(PIN_BUMPER)) { // Lee el pin del bumper y del sensor infrarojo, y si detecta algo sigue el programa
             // if (input(PIN_INF)) {
             //    break;
             // }
